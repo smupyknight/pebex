7de2ea3082c5d278fe36b9a0f4e1259b3ad11418
unknown <smupyknight>
2018-09-17 01:25:46 +0800

    Initial commit

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x35, 0xe3, 0x67, 0x94, 0x74, 0x93, 0xa6, 0x83, 0x74, 0x71, 0x62, 0x37, 0xc6, 0xd7, 0x1d, 0x36 } };
+}
