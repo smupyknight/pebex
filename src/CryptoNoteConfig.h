bba3197bc6050b97e9eae185449a4d6c53587424
unknown <smupyknight>
2018-09-18 21:27:17 +0800

    Update seed nodes

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "PEBEX";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff000101029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210166b02afcc0dcdbcf302dad2a76c6ebe54c1045ece07e901871895ddaeb49204a";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff000101029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121011a905d8433a9665de71a36bf123c9315b39ea25bede83627c535283260802ddf";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
@@ -107,6 +107,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
   "198.23.60.17:49212",
+  "45.32.119.203:49212",
+  "207.148.122.66:49212",
   //"your_seed_ip1.com:8080",
   //"your_seed_ip2.com:8080",
 };
